a9f9505834eccbf0a0dcfdcc98534d0506384bc3
Felipe <fen1499@gmail.com>
2021-03-09 06:48:21 -0300

    Inital commit

--- mix.exs
+++ mix.exs
@@ -51,7 +51,6 @@ defmodule Rocketpay.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:credo, "~>1.5", only: [:dev, :test], runtime: false},
-      {:bcrypt_elixir, "~>2.0"},
       {:excoveralls, "~>0.10", only: :test}
     ]
   end
